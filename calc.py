91bac7cad70c8152c9eb5fb5359c9264e926a6fd
CloudLining <ankur5yk@gmail.com>
2024-04-22 22:02:42 +0530

    python-jenkins-1

---
+++ calc.py
@@ -0,0 +1,13 @@
+class calculator:
+    def __init__(self, var1, var2):
+        self.var1 = var1
+        self.var2 = var2
+        print("calculator is called")
+
+    def add(self):
+        return (self.var1 + self.var2)
+
+    def multiply(self):
+        return self.var1*self.var2
+    
+
